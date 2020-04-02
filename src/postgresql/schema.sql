e594e647884db36476ae47d7c007ebd4175ca313
Sean Lorman <sean.lorman@gmail.com>
2020-04-02 15:08:15 -0600

    Change table details to match the .csv files and added the .csv files to the project and they are already in .gitignore

--- src/postgresql/schema.sql
+++ src/postgresql/schema.sql
@@ -5,29 +5,29 @@ CREATE DATABASE qa;
 \c qa;
 
 CREATE TABLE questions (
-    question_id SERIAL PRIMARY KEY,
-    product_id INT,
-    question_body VARCHAR(1000) NOT NULL,
-    question_date_written DATE,
-    question_asker_name VARCHAR(60) NOT NULL,
-    question_asker_email VARCHAR(60) NOT NULL,
-    question_reported INT NOT NULL,
-    question_helpful INT NOT NULL
+    id SERIAL PRIMARY KEY NOT NULL,
+    product_id INT NOT NULL,
+    body VARCHAR(1000) NOT NULL,
+    date_written DATE NOT NULL,
+    asker_name VARCHAR(60) NOT NULL,
+    asker_email VARCHAR(60) NOT NULL,
+    reported INT NOT NULL,
+    helpful INT NOT NULL
 );
 
 CREATE TABLE answers (
-    answer_id SERIAL PRIMARY Key,
+    id SERIAL PRIMARY Key,
     question_id INT REFERENCES questions(question_id),
-    answer_body VARCHAR(1000) NOT NULL,
-    answer_date_written DATE,
-    answer_name VARCHAR(60) NOT NULL,
-    answer_email VARCHAR(60) NOT NULL,
-    answer_reported INT NOT NULL,
-    answer_helpful INT NOT NULL,
+    body VARCHAR(1000) NOT NULL,
+    date_written DATE,
+    answerer_name VARCHAR(60) NOT NULL,
+    answerer_email VARCHAR(60) NOT NULL,
+    reported INT NOT NULL,
+    helpful INT NOT NULL,
 );
 
 CREATE TABLE answers_photos (
-    answer_photo_id SERIAL PRIMARY KEY,
-    answer_photo_answers_id INT REFERENCES answers(answer_id),
-    answer_photo_url TEXT[5]
+    id SERIAL PRIMARY KEY,
+    answers_id INT REFERENCES answers(answer_id),
+    url TEXT[5]
 );
