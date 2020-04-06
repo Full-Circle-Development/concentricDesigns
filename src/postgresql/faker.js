46dfe75f40a3326eab651721b6a6afd01fe9a89c
Sean Lorman <sean.lorman@gmail.com>
2020-04-06 16:14:04 -0600

    Have the csv looking exactly how it should when produced

--- src/postgresql/faker.js
+++ src/postgresql/faker.js
@@ -1,102 +1,25 @@
-const fs = require("fs");
-const faker = require("faker");
+var http = require("http");
+var fs = require("fs");
 
-// let randomName = faker.name.findName();
-// include node fs module
-let data = [
-  // [
-  //   "id",
-  //   "product_id",
-  //   "body",
-  //   "date_written",
-  //   "asker_name",
-  //   "asker_email",
-  //   "reported",
-  //   "helpful",
-  // ],
-  [
-    3521635,
-    1000012,
-    faker.lorem.sentence(),
-    faker.date.recent(),
-    faker.name.firstName() + faker.name.lastName(),
-    faker.internet.email(),
-    0,
-    1,
-  ],
-];
-// writeFile function with filename, content and callback function
+http
+  .createServer(function (req, res) {
+    // This opens up the writeable stream to `output`
+    var writeStream = fs.createWriteStream("./output");
 
-const download_csv = () => {
-  var csv =
-    "id, product_id, body, date_written, asker_name, asker_email, reported, helpful,\n";
-  data.forEach(function (row) {
-    csv += row.join(",");
-    csv += "\n";
-  });
-  // console.log(data);
-  console.log(csv);
-  fs.writeFile("/qa.csv", csv, function (err) {
-    if (err) throw err;
-    console.log("File is created successfully.");
-  });
-};
+    // This pipes the POST data to the file
+    req.pipe(writeStream);
 
-download_csv();
-// () => console.log(data));
-//   console.log(csv);
-//};
+    // After all the data is saved, respond with a simple html form so they can post more data
+    req.on("end", function () {
+      res.writeHead(200, { "content-type": "text/html" });
+      res.end(
+        '<form method="POST"><input name="test" /><input type="submit"></form>'
+      );
+    });
 
-// fs.writeFile("newfile.csv", data, function (err) {
-//   if (err) throw err;
-//   console.log("File is created successfully.");
-// });
-
-// {
-//   /* <script> */
-// }
-// let data = ["test"];
-// let data = [
-//   [
-//     "id",
-//     "product_id",
-//     "body",
-//     "date_written",
-//     "asker_name",
-//     "asker_email",
-//     "reported",
-//     "helpful",
-//   ],
-//   [
-//     3521635,
-//     1000012,
-//     faker.lorem.sentence(),
-//     faker.date.recent(),
-//     faker.name.firstName() + faker.name.lastName(),
-//     faker.internet.email(),
-//     0,
-//     1,
-//     // faker.fake(
-//     //   "{{lorem.sentence}}, {{date.recent}}, {{name.firstName name.lastName}}, {{internet.email}}, 0, 1"
-//     // ),
-//   ],
-// ];
-// var data = [
-//   ["Foo", "programmer"],
-//   ["Bar", "bus driver"],
-//   ["Moo", "Reindeer Hunter"],
-// ];
-
-// const download_csv = () => {
-//   var csv = "Name,Title\n";
-//   data.forEach(function (row) {
-//     csv += row.join(",");
-//     csv += "\n";
-//   });
-// fs.writeFile("/test.csv", data, () => console.log(data));
-//   console.log(csv);
-// };
-
-// download_csv();
-// </script>
-// console.log(randomName);
+    // This is here incase any errors occur
+    writeStream.on("error", function (err) {
+      console.log(err);
+    });
+  })
+  .listen(8080);
--- src/postgresql/faker2.js
+++ src/postgresql/faker2.js
@@ -1,32 +1,33 @@
 const fs = require("fs");
 const faker = require("faker");
+// 2018-01-04T00:00:00.000Z
+let record = 3521635;
+let product_id = 1000012;
 
 var bigSet = [];
 
-let csv = "body, date_written, asker_name, asker_email\n";
+let csv =
+  "id, product_id, body, date_written, asker_name, asker_email, reported, helpful\n";
 
-for (var i = 20; i >= 18; i--) {
-  let dataRow = `${faker.lorem.sentence()}, ${faker.date.recent()}, ${faker.name.firstName()}, ${faker.name.lastName()}, ${faker.internet.email()}\n`;
+for (var i = record; i <= 3521645; i++, product_id++) {
+  product_id;
+  let dataRow =
+    i +
+    "," +
+    product_id +
+    "," +
+    `"${faker.lorem.sentence()}", "${faker.date
+      .between("2018-01-01", "2020-04-05")
+      .toJSON()
+      .slice(
+        0,
+        10
+      )}", "${faker.name.firstName()} ${faker.name.lastName()}", "${faker.internet.email()}", ${faker.random.number(
+      1
+    )}, ${faker.random.number(15)}\n`;
   csv += dataRow;
 }
 bigSet.push(csv);
-
-console.log(bigSet);
-
-// download = () => {
-//   var csv = "body, date_written, asker_name, asker_email,\n";
-//   bigSet.forEach(function (row) {
-//     csv += row.join(",");
-//     csv += "\n";
-//     row += "\n";
-//   });
-// };
-
-// download();
-// bigSet.forEach(function (row) {
-//   csv += row.join(",");
-//   csv += "\n";
-//});
 
 fs.writeFile(__dirname + "/bigDataSet.csv", bigSet, function () {
   console.log("bigDataSet generated successfully!");
