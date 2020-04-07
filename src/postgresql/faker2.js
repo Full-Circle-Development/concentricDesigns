8720ba51ba4775fa37f69be38f3f9defa12ef4ba
Sean Lorman <sean.lorman@gmail.com>
2020-04-06 19:12:52 -0600

    have a .csv file to get to 10M primary records, but not enough room on machine to loud into DB. Going to figure out how to increase memory size.

--- src/postgresql/faker2.js
+++ src/postgresql/faker2.js
@@ -1,15 +1,15 @@
 const fs = require("fs");
 const faker = require("faker");
-// 2018-01-04T00:00:00.000Z
 let record = 3521635;
 let product_id = 1000012;
 
 var bigSet = [];
 
+let streamy = fs.createWriteStream("./bigDataQ.csv");
 let csv =
   "id, product_id, body, date_written, asker_name, asker_email, reported, helpful\n";
-
-for (var i = record; i <= 3521645; i++, product_id++) {
+streamy.write(csv);
+for (var i = record; i <= 10000000; i++, product_id++) {
   product_id;
   let dataRow =
     i +
@@ -25,10 +25,11 @@ for (var i = record; i <= 3521645; i++, product_id++) {
       )}", "${faker.name.firstName()} ${faker.name.lastName()}", "${faker.internet.email()}", ${faker.random.number(
       1
     )}, ${faker.random.number(15)}\n`;
-  csv += dataRow;
+  streamy.write(dataRow);
+  // csv += dataRow;
 }
-bigSet.push(csv);
-
-fs.writeFile(__dirname + "/bigDataSet.csv", bigSet, function () {
-  console.log("bigDataSet generated successfully!");
-});
+// bigSet.push(csv);
+streamy.end();
+// fs.writeFile(__dirname + "/bigDataSet.csv", bigSet, function () {
+//   console.log("bigDataSet generated successfully!");
+// });
