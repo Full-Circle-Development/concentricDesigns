ad55976b6893f7344ef0d742e261308283c00768
Sean Lorman <sean.lorman@gmail.com>
2020-04-12 21:07:31 -0600

    Postman not returning the GET request for all answers yet, but the query seems good so just need to figure out the code after receiving the data back

--- postgresql/pgconfig.js
+++ postgresql/pgconfig.js
@@ -107,25 +107,77 @@ const getAllAnswers = (question_id, page, count) => {
         let resRows = res.rows;
         let resultObj = {
           question: values[0],
-          page: Number(values[1]) || 0,
-          count: Number(values[2]) || 5,
+          page: 0, //Number(values[1]) || 0,
+          count: 5, //Number(values[2]) || 5,
           results: [],
         };
         let photosArr = [];
-        let answerObj = {};
+        // let answerObj = {};
 
         for (let i = 0; i < resRows.length; i++) {
+          let answerObj = {
+            answer_id: resRows[i].answer_id,
+            body: resRows[i].answer_body,
+            day: resRows[i].answer_date,
+            answerer_name: resRows[i].answerer_name,
+            helpfulness: resRows[i].answer_helpfulness,
+            photos: [],
+          };
+
           let photosObj = {};
-          if (resRows.url) {
+          if (resRows.photo_id) {
             photosObj[id] = resRows.photo_id;
             photosObj[url] = resRows.url;
           }
-          photosArr.push(photosObj);
+          answersObj.push(photosObj);
+
+          //if (resRows.answer_id) {
+          //}
+          resultObj.results.push(answerObj);
+          photosObj = {};
+          answerObj = {};
         }
+        return resultObj;
       })
       .catch((err) => err)
   );
 };
+// ex
+/*
+{
+  "question": "1",
+  "page": 0,
+  "count": 5,
+  "results": [
+    {
+      "answer_id": 8,
+      "body": "What a great question!",
+      "date": "2018-01-04T00:00:00.000Z",
+      "answerer_name": "metslover",
+      "helpfulness": 8,
+      "photos": [],
+    },
+    {
+      "answer_id": 5,
+      "body": "Something pretty durable but I can't be sure",
+      "date": "2018-01-04T00:00:00.000Z",
+      "answerer_name": "metslover",
+      "helpfulness": 5,
+      "photos": [{
+          "id": 1,
+          "url": "urlplaceholder/answer_5_photo_number_1.jpg"
+        },
+        {
+          "id": 2,
+          "url": "urlplaceholder/answer_5_photo_number_2.jpg"
+        },
+        // ...
+      ]
+    },
+    // ...
+  ]
+}
+*/
 
 // POST A QUESTION
 
