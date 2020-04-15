0c925f616d141bf55ef0d4e23eab136777861779
Sean Lorman <sean.lorman@gmail.com>
2020-04-15 17:40:28 -0600

    Index DB and right before I redo my GET query

--- script.js
+++ script.js
@@ -2,6 +2,33 @@ import http from "k6/http";
 import { check, sleep } from "k6";
 import { Rate } from "k6/metrics";
 
+// Stress Test Ramping up
+// export let options = {
+//   stages: [
+//     { duration: "2m", target: 100 }, // below normal load
+//     { duration: "5m", target: 100 },
+//     { duration: "2m", target: 200 }, // normal load
+//     { duration: "5m", target: 200 },
+//     { duration: "2m", target: 300 }, // around the breaking point
+//     { duration: "5m", target: 300 },
+//     { duration: "2m", target: 400 }, // beyond the breaking point
+//     { duration: "5m", target: 400 },
+//     { duration: "10m", target: 0 }, // scale down. Recovery stage.
+//   ],
+// };
+
+// export default function () {
+//   const BASE_URL = "http://localhost:3000"; // make sure this is not production
+
+//   let responses = http.batch([
+//     ["GET", `${BASE_URL}/qa/1/`, null, { tags: { name: "PublicCrocs" } }],
+//     ["GET", `${BASE_URL}/qa/2/`, null, { tags: { name: "PublicCrocs" } }],
+//     ["GET", `${BASE_URL}/qa/3/`, null, { tags: { name: "PublicCrocs" } }],
+//     ["GET", `${BASE_URL}/qa/4/`, null, { tags: { name: "PublicCrocs" } }],
+//   ]);
+
+//   sleep(1);
+// }
 // GET ROUTES
 
 export let errorRate = new Rate("errors");
