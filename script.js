aa0d5d6f5cff56919e924cdb39460cb0d50496e3
Sean Lorman <sean.lorman@gmail.com>
2020-04-14 20:19:00 -0600

    Add a script.js for a GET and POST request for k6

--- script.js
+++ script.js
@@ -4,40 +4,41 @@ import { Rate } from "k6/metrics";
 
 // GET ROUTES
 
-// export let errorRate = new Rate("errors");
-
-// export default function () {
-//   var url = "http://localhost:3000/qa/1";
-//   var params = {
-//     headers: {
-//       "Content-Type": "application/json",
-//     },
-//   };
-//   check(http.get(url, params), {
-//     "status is 200": (r) => r.status == 200,
-//   }) || errorRate.add(1);
-// }
-
-// POST ROUTES
-
 export let errorRate = new Rate("errors");
 
 export default function () {
-  var url = "http://localhost:3000/qa/100";
+  var url = "http://localhost:3000/qa/1";
   var params = {
     headers: {
       "Content-Type": "application/json",
     },
   };
-
-  var data = JSON.stringify({
-    body: `If I had two magic boxes I would give one to Gabe and the other to user+${__VU}`,
-    name: `user+${__VU}`,
-    email: `user+${__VU}@mail.com`,
-  });
-  check(http.post(url, data, params), {
-    "status is 201": (r) => r.status == 201,
+  check(http.get(url, params), {
+    "status is 200": (r) => r.status == 200,
   }) || errorRate.add(1);
-
-  sleep(1);
+  sleep(0.001); // changed from 1 since a µs is .001 ms
 }
+
+// POST ROUTES
+
+// export let errorRate = new Rate("errors");
+
+// export default function () {
+//   var url = "http://localhost:3000/qa/100";
+//   var params = {
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//   };
+
+//   var data = JSON.stringify({
+//     body: `If I had two magic boxes I would give one to Gabe and the other to user+${__VU}`,
+//     name: `user+${__VU}`,
+//     email: `user+${__VU}@mail.com`,
+//   });
+//   check(http.post(url, data, params), {
+//     "status is 201": (r) => r.status == 201,
+//   }) || errorRate.add(1);
+
+//   sleep(1);
+// }
